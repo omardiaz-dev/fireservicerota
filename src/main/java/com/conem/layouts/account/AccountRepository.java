fd031e33f60471d06f6491537947e6ea97f156cb
omardiaz-dev <omar@kinedu.com>
2021-05-28 08:38:48 -0500

    Empty branch
    Empty Branch

--- src/main/java/com/conem/layouts/account/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.conem.layouts.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-	Account findOneByEmail(String email);
-}
--- src/main/java/com/conem/layouts/models/dtos/GenericDTO.java
+++
@@ -1,9 +0,0 @@
-package com.conem.layouts.models.dtos;
-
-import java.io.Serializable;
-
-public class GenericDTO implements Serializable{
-
-	private static final long serialVersionUID = 9221165715663135282L;
-
-}
